0fe15737be530b48250ed7cd1f234ad43605e64a
Ray Alez <raymestalez@gmail.com>
2017-11-22 05:26:46 -0800

    Update.

--- client/pwa/sw.js
+++ client/pwa/sw.js
@@ -33,7 +33,7 @@ self.addEventListener('activate', function(event) {
 		    // Return true if you want to remove this cache,
 		    // but remember that caches are shared across
 		    // the whole origin
-		    return true
+		    return !cacheName.includes('profile')
 		}).map(function(cacheName) {
 		    return caches.delete(cacheName);
 		})
